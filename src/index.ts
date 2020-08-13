7a1f55900103864c94abda1cd56ab8bed93c1cd6
Maarten van Meersbergen <maartenvm@gmail.com>
2020-08-13 10:26:54 +0200

    work-in-progress to rewrite to jupyterlab extension and typescript

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+import provbook from './provbook';
+// import { NotebookPanel, Notebook, INotebookTracker } from '@jupyterlab/notebook';
+
+/**
+ * Initialization data for the provbook extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'provbook',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('the JupyterLab main application:', app);
+    provbook.register_provbook();
+  }  
+};
+
+export default extension;
+
+
+// /**
+//  * Initialization data for the jupyterlab_nbprovenance extension.
+//  */
+// const plugin: JupyterFrontEndPlugin<void> = {
+//   id: 'jupyterlab_nbprovenance',
+//   autoStart: true,
+//   requires: [ILayoutRestorer, INotebookTracker],
+//   activate,
+// };
+
+// export default plugin;
+
+// export const notebookModelCache = new Map<Notebook, NotebookProvenance>();
+
+// function activate(app: JupyterLab, restorer: ILayoutRestorer, nbTracker: INotebookTracker): void {
+//   nbTracker.widgetAdded.connect((_: INotebookTracker, nbPanel: NotebookPanel) => {
+//     // wait until the session with the notebook model is ready
+//     nbPanel.sessionContext.ready.then(() => {
+//       const notebook: Notebook = nbPanel.content;
+//       if (!notebookModelCache.has(notebook)) {
+//         notebookModelCache.set(notebook, new NotebookProvenance(app, notebook));
+//       }
+//     });
+//   });
+
+//   const provenanceView = new SideBar(app.shell, nbTracker);
+//   provenanceView.id = 'nbprovenance-view';
+//   provenanceView.title.label = 'Notebook Provenance';
+//   provenanceView.title.closable = true;
+//   // provenanceView.title.iconRenderer = 'jp-nbprovenanceIcon';
+//   // provenanceView.title.iconClass = 'jp-nbprovenanceIcon';
+
+//   restorer.add(provenanceView, 'nbprovenance_view');
+
+//   // Rank has been chosen somewhat arbitrarily
+//   app.shell.add(provenanceView, 'left', { rank: 700 });
+// }
